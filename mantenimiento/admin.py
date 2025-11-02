c40377715fdd2d61297d1c64c2c67da31230d77a
alisho0 <alejorea2015@gmail.com>
2025-11-01 23:24:01 -0300

    Panel de admin y dos datos insertados

--- mantenimiento/admin.py
+++ mantenimiento/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OrdenDeTrabajo, Suministro, ConsumoSuministro
 
 # Register your models here.
+
+admin.site.register(OrdenDeTrabajo)
+admin.site.register(Suministro)
+admin.site.register(ConsumoSuministro)
